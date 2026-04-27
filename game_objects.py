7ad605bff4975359d35e2af277b265a17600eb68
rishajava <rishajava@gmail.com>
2026-04-27 20:06:52 +0300

    Добавлена аннотация типов и документация.

--- game_objects.py
+++ game_objects.py
@@ -2,7 +2,8 @@ from questions import *
 from typing import Literal
 
 class Button:
-    def __init__(self, x, y, width, height, text, color, text_color=BLACK, font_size=36):
+    '''Простой класс кнопки. Имеет отрисовку и обработку наводки/нажатия.'''
+    def __init__(self, x : int, y : int, width : int, height : int, text : str, color : tuple[int, int, int], text_color : tuple[int, int, int] = BLACK, font_size : int = 36):
         # коллайдер
         self.rect = pygame.Rect(x, y, width, height)
         # цвет кнопки(если использовать колайдер, как кнопку
@@ -15,24 +16,27 @@ class Button:
         self.font = pygame.font.Font(None, font_size)
         # Is на кнопку наведён курсор?
         self.is_hovered = False
-        
-    def draw(self, surface):
-        # чуть-чуть осветляем кнопку, если is_hovered=True
+
+    def draw(self, surface : pygame.Surface) -> None:
+        '''Отрисовывает кнопку, учитывая наводку курсором.
+        Принимает поверхность, на которой рисуем. Возвращает None.'''
         current_color = self.color
+        # меняем цвет кнопки, если is_hovered=True
         if self.is_hovered:
             # current_color = tuple(min(c + 50, 255) for c in self.color)
             current_color = GREEN
-        
+
         pygame.draw.rect(surface, current_color, self.rect , border_radius=12)
         pygame.draw.rect(surface, BLACK, self.rect, 2, border_radius=12)
-        
+
         # делаем surface с текстом на нём
         text_surf = self.font.render(self.text, True, self.text_color)
         text_rect = text_surf.get_rect(center=self.rect.center)
         surface.blit(text_surf, text_rect)
-        
-    # событие нажатия на кнопку
-    def handle_event(self, event):
+
+    def handle_event(self, event : pygame.Event) -> bool:
+        '''Обрабатывает события, связанные с кнопкой.
+        Принимает событие. Возвращает True, если кнопка была нажата, иначе - False.'''
         # если событие - движение мыши, то изменияем состояние наведённости на кнопку
         if event.type == pygame.MOUSEMOTION:
             self.is_hovered = self.rect.collidepoint(event.pos)
@@ -41,20 +45,28 @@ class Button:
                 return True  # если кнопка нажат ЛКМ, то возвращаем true
         return False
 
-
 class Territory:
-    def __init__(self, x, y, row, col):
+    '''Класс "территории" - представляет из себя одну ячейку, которую можно захватить. Поддерживает отрисовку и работу с владельцем территории.
+    Хранит положение прямоугольника, номер строки и столбца на карте и владельца.'''
+    def __init__(self, x : int, y : int, row : int, col : int):
+        # коллайдер
         self.rect = pygame.Rect(x, y, CELL_SIZE - 5, CELL_SIZE - 5)
+        # номер строки
         self.row = row
+        # номер столбца
         self.col = col
+        # владелец
         self.owner = NOBODY
+        # цвет
         self.color = GRAY
 
-    def draw(self, surface):
+    def draw(self, surface : pygame.Surface) -> None:
+        '''Отрисовка. Пока прямоугольник с обводкой.'''
         pygame.draw.rect(surface, self.color, self.rect)
         pygame.draw.rect(surface, BLACK, self.rect, width=2)
 
-    def set_owner(self, owner : Literal[NOBODY, PLAYER, BOT]):
+    def set_owner(self, owner : Literal[NOBODY, PLAYER, BOT]) -> None:
+        '''Сеттер. Задаёт владельца территории.'''
         self.owner = owner
         if owner == PLAYER:
             self.color = RED
@@ -65,14 +77,16 @@ class Territory:
 
 
 class Map:
-    def __init__(self, surface, col_count, row_count):
-        self.surface = surface
+    '''Класс карты. Пока это просто row_count на col_count ячеек территории.'''
+    def __init__(self, surface : pygame.Surface, col_count : int, row_count : int):
         self.grid = []
+        self.surface = surface
         self.row_count = row_count
         self.col_count = col_count
         self.create_grid()
 
-    def create_grid(self):
+    def create_grid(self) -> None:
+        '''Создаёт решётку.'''
         start_x = (WIDTH - (CELL_SIZE * self.col_count)) // 2
         start_y = (HEIGHT - (CELL_SIZE * self.row_count)) // 2
 
@@ -84,7 +98,8 @@ class Map:
                 row.append(Territory(x, y, r, c))
             self.grid.append(row)
 
-    def get_territories(self, owner : Literal[NOBODY, PLAYER, BOT]):
+    def get_territories(self, owner : Literal[NOBODY, PLAYER, BOT]) -> Territory:
+        '''Возвращает список территорий, принадлежащих owner.'''
         territories = []
         for row in self.grid:
             for territory in row:
@@ -92,22 +107,25 @@ class Map:
                     territories.append(territory)
         return territories
 
-    def reset(self):
+    def reset(self) -> None:
+        '''Пересоздаём решётку. Нужно для перезапуска игры.'''
         self.grid = []
         self.create_grid()
 
-    def draw(self):
+    def draw(self) -> None:
+        '''Отрисовка всех территорий на surface.'''
         for row in self.grid:
             for territory in row:
                 territory.draw(self.surface)
 
 
 class Game:
+    '''Основной класс игры.'''
     EXPLORATION = 0
     FIGHTING = 1
     GAME_OVER = 2
 
-    def __init__(self, surface, col_count, row_count):
+    def __init__(self, surface : pygame.Surface, col_count : int, row_count : int) -> None:
         self.surface = surface
         self.battlefield = Map(surface, col_count, row_count)
 
@@ -145,12 +163,14 @@ class Game:
         return False
 
     # запускает сообщение ботяры
-    def show_temporary_message(self, text):
+    def show_temporary_message(self, text : str) -> None:
+        '''Вспомогательная функция, обновляющая текст сообщения игры.'''
         self.show_message = True
         self.message_text = text
         self.message_timer = pygame.time.get_ticks()
 
     def bot_move(self):
+        '''Функция, выполняющая ход бота.'''
         empty = self.battlefield.get_territories(NOBODY)
         if empty:
             self.selected_territory = random.choice(empty)
@@ -169,7 +189,8 @@ class Game:
         else:
             self.check_win()
 
-    def handle_click(self, pos):
+    def handle_click(self, pos : tuple[int, int]) -> None:
+        '''Смотрит, куда походил игрок. Обновляет состояние игры в зависимости от данных.'''
         if self.current_state == self.GAME_OVER or self.waiting_for_quiz or self.show_message:
             return
 
@@ -183,7 +204,8 @@ class Game:
                         self.quiz.new_question()
                         return
 
-    def capture_territory(self, territory, owner):
+    def capture_territory(self, territory : Territory, owner : Literal[NOBODY, PLAYER, BOT]) -> None:
+        '''Захват территории :-). Меняет владельца территории territory на owner и сменяет ход.'''
         territory.set_owner(owner)
         self.check_win()
         # смена хода
@@ -191,7 +213,8 @@ class Game:
             self.current_turn = BOT if owner == PLAYER else PLAYER
 
     # делается то, что не зависит от игрока
-    def update(self):
+    def update(self) -> None:
+        '''Обновляет всё, что не зависит от игрока.'''
         if self.current_state == self.GAME_OVER:
             return
 
@@ -210,7 +233,8 @@ class Game:
         else:
             self.bot_timer = 0
 
-    def draw(self):
+    def draw(self) -> None:
+        '''Отрисовка всей игры.'''
         self.surface.fill(WHITE)
 
         self.battlefield.draw()
@@ -268,7 +292,8 @@ class Game:
             self.surface.blit(win_text, win_rect)
             self.surface.blit(sub_text, sub_rect)
 
-    def handle_quiz_result(self, is_correct):
+    def handle_quiz_result(self, is_correct : bool) -> None:
+        '''Обновляет данные по результату квиза.'''
         if is_correct:
             if self.selected_territory:
                 self.capture_territory(self.selected_territory, PLAYER)
@@ -280,8 +305,8 @@ class Game:
         self.waiting_for_quiz = False
         self.selected_territory = None
 
-    # отчистка для перезапуска игры
-    def reset(self):
+    def reset(self) -> None:
+        '''Отчистка для перезапуска игры.'''
         self.current_turn = random.choice([PLAYER, BOT])
         self.winner = NOBODY
         self.waiting_for_quiz = False
@@ -296,8 +321,8 @@ class Game:
         self.current_state = 0
         self.battlefield.reset()
 
-    # обновляем от события
-    def event_update(self, event):
+    def event_update(self, event : pygame.Event) -> None:
+        '''Обновляем атрибуты в зависимости от события event.'''
         if self.waiting_for_quiz:
             result = self.quiz.handle_event(event)
             if result is not None:
