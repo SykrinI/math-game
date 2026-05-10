e8a0d2bf8b2113056df184e00a71b3cb53fbb96d
rishajava <rishajava@gmail.com>
2026-05-11 01:07:17 +0300

    Убраны GRID_WIDTH и GRID_HEIGHT. Добавлены новые цвета.

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ FPS = 60
 
 # параметры карты
 CELL_SIZE = 80
-GRID_WIDTH = 3
-GRID_HEIGHT = 2
 
 # id участников
 NOBODY = 0
@@ -24,3 +22,4 @@ YELLOW = (255, 255, 0)
 LIGHT_YELLOW = (255, 255, 173)
 GRAY = (128, 128, 128)
 LIGHT_BLUE = (173, 216, 230)
+PURPLE = (139, 0, 255)
